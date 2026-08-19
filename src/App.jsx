5e495fe7d3f7c181bd92c9c4bd0b140054bd1119
Tan Kai Yi <xiaopihaikaiyi@gmail.com>
2026-08-19 17:57:08 +0800

    feat: create ImageSlider Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Accordion from './components/accordion/Accordion'
 import RandomColor from './components/random_color/RandomColor'
 import StarRating from './components/star_rating/StarRating'
+import ImageSlider from './components/image_slider/ImageSlider'
 
 const App = () => {
   return (
     <>
       {/* <Accordion /> */}
       {/* <RandomColor /> */}
-      <StarRating />
+      {/* <StarRating /> */}
+      <ImageSlider />
     </>
   )
 }
---
+++ src/components/image_slider/ImageSlider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+const ImageSlider = () => {
+  return (
+    <div className="imageSliderContainer">ImageSlider</div>
+  )
+}
+
+export default ImageSlider
