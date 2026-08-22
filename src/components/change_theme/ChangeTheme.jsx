fc4091a38758fde39f02f3f7202a3dbb29a36437
Tan Kai Yi <xiaopihaikaiyi@gmail.com>
2026-08-23 00:16:59 +0800

    feat: implement changeTeam component

--- src/components/change_theme/ChangeTheme.jsx
+++ src/components/change_theme/ChangeTheme.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./style.css"
+import useLocalStorage from './useLocalStorage'
 
 const ChangeTheme = () => {
+    const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+    function handleToggleTheme() {
+        setTheme(theme == "light" ? "dark" : "light");
+    }
+
+    console.log(theme);
+
     return (
-        <div className="changeThemeContainer">
-            ChangeTheme
+        <div className="changeThemeContainer" data-theme={theme}>
+            <div className="contextContainer">
+                <p>Hello World!</p>
+                <button className="changeThemeButton" onClick={() => { handleToggleTheme() }}>Change Theme</button>
+            </div>
         </div>
     )
 }
