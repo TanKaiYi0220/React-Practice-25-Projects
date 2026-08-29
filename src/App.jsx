3be5747458e6afc9ad40ef96079c820952843f8f
Tan Kai Yi <xiaopihaikaiyi@gmail.com>
2026-08-30 01:29:07 +0800

    feat: create Tabs component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import TreeView from './components/tree-view/TreeView'
 import QRCodeGenerator from './components/qr-code/QRCodeGenerator'
 import ChangeTheme from './components/change_theme/ChangeTheme'
 import ScrollIndicator from './components/scroll_indicator/ScrollIndicator'
+import Tabs from './components/tabs/Tabs'
 
 const App = () => {
   return (
@@ -22,7 +23,8 @@ const App = () => {
       {/* <TreeView /> */}
       {/* <QRCodeGenerator /> */}
       {/* <ChangeTheme /> */}
-      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
+      <Tabs />
     </>
   )
 }
---
+++ src/components/tabs/Tabs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const Tabs = () => {
+    return (
+        <div className="tabsContainer">
+            Tabs
+        </div>
+    )
+}
+
+export default Tabs
